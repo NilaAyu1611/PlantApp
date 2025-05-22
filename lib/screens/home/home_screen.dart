908d3ef3c164f56ffaced80fd609ada0e0426c46
NilaAyu1611 <nilaayucw@gmail.com>
2025-05-23 00:16:04 +0700

    buat appbar di homescreen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:plantapp/components/my_butttom_nav_bar.dart';
+import 'package:plantapp/constants.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: kBackgroundColor,
+      appBar: buildAppBar(),
+      body: Body(),
+      bottomNavigationBar: MyBottomNavBar(),
+    );
+  }
+
+   AppBar buildAppBar() {
+    return AppBar(
+      backgroundColor: kPrimaryColor,
+      elevation: 0,
+      leading: IconButton(
+        icon: SvgPicture.asset(
+          "assets/icons/menu.svg",
+          color: Colors.white, 
+        ),
+        onPressed: () {},
+      ),
+    );
   }
 }
