e60c709b1abeb9f34ddfe8232a76f3c45f16ad74
NilaAyu1611 <nilaayucw@gmail.com>
2025-05-23 02:15:41 +0700

    Buat widget ProfileHeader dng struktur dasar UI dart

---
+++ lib/screens/home/components/profile_header.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ProfileHeader extends StatefulWidget {
+  const ProfileHeader({super.key});
+
+  @override
+  State<ProfileHeader> createState() => _ProfileHeaderState();
+}
+
+class _ProfileHeaderState extends State<ProfileHeader> {
+   File? _image;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
