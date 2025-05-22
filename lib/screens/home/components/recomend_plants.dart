5a24acbc2fa9ed15d6b88842fb4831d42b5b77d5
NilaAyu1611 <nilaayucw@gmail.com>
2025-05-23 01:02:20 +0700

    membuat list dari recomendsplants

---
+++ lib/screens/home/components/recomend_plants.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:plantapp/screens/details/details_screen.dart';
+
+
+import '../../../constants.dart';
+
+class RecomendsPlants extends StatelessWidget {
+  const RecomendsPlants({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: <Widget>[
+          RecomendPlantCard(
+            image: "assets/images/image_1.png",
+            title: "Samantha",
+            country: "Russia",
+            price: 440,
+            press: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DetailsScreen(),
+                ),
+              );
+            },
+          ),
+          RecomendPlantCard(
+            image: "assets/images/image_2.png",
+            title: "Angelica",
+            country: "Russia",
+            price: 440,
+            press: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DetailsScreen(),
+                ),
+              );
+            },
+          ),
+          RecomendPlantCard(
+            image: "assets/images/image_3.png",
+            title: "Samantha",
+            country: "Russia",
+            price: 440,
+            press: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
