df9099008f9eb2bec6a4cb9ab036bba33d71525c
NilaAyu1611 <nilaayucw@gmail.com>
2025-05-23 00:51:50 +0700

    tambah TitleWithCustomUnderline widget untuk styled section titles

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:plantapp/constants.dart';
+import 'package:plantapp/screens/home/components/header_with_seachbox.dart';
+import 'package:plantapp/screens/home/components/tittle_with_more_bbtn.dart';
 
 class Body extends StatelessWidget {
   const Body({super.key});
@@ -15,7 +17,12 @@ class Body extends StatelessWidget {
         padding: const EdgeInsets.all(kDefaultPadding),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: const <Widget>[            
+          children: const <Widget>[    
+            HeaderWithSearchBox(size: size),
+            TitleWithMoreBtn(title: "Recomended", press: () {}),
+           
+            TitleWithMoreBtn(title: "Featured Plants", press: () {}),
+             
             SizedBox(height: kDefaultPadding),
           ],
         ),
