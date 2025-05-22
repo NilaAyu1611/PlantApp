b5e716138e08383e055d44bdb7b39f002623882d
NilaAyu1611 <nilaayucw@gmail.com>
2025-05-23 01:10:38 +0700

    komponen card yang tampilin gambar (bisa diklik)

--- lib/screens/home/components/featurred_plants.dart
+++ lib/screens/home/components/featurred_plants.dart
@@ -25,3 +25,38 @@ class FeaturedPlants extends StatelessWidget {
     );
   }
 }
+
+class FeaturePlantCard extends StatelessWidget {
+  const FeaturePlantCard({
+    super.key,
+    required this.image,
+    required this.press,
+  });
+
+  final String image;
+  final VoidCallback press; 
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return GestureDetector(
+      onTap: press,
+      child: Container(
+        margin: const EdgeInsets.only(
+          left: kDefaultPadding,
+          top: kDefaultPadding / 2,
+          bottom: kDefaultPadding / 2,
+        ),
+        width: size.width * 0.8,
+        height: 185,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage(image),
+          ),
+        ),
+      ),
+    );
+  }
+}
