e7bd2cee8ef85e77794b05d60614d334de4fb5cb
NilaAyu1611 <nilaayucw@gmail.com>
2025-05-23 01:35:56 +0700

    Tambah struktur dasar widget ImageAndIcons

---
+++ lib/screens/details/components/image_and_icons.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ImageAndIcons extends StatelessWidget {
+  const ImageAndIcons({
+    Key? key,
+    required this.size,
+  }) : super(key: key);
+
+  final Size size;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
