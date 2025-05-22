d0c0b0deeeebee9e9260caf2900828bd12c0113c
NilaAyu1611 <nilaayucw@gmail.com>
2025-05-23 00:39:17 +0700

    Buat TitleWithMoreBtn widget dengan title and More button

---
+++ lib/screens/home/components/tittle_with_more_bbtn.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import '../../../constants.dart';
+
+class TitleWithMoreBtn extends StatelessWidget {
+  const TitleWithMoreBtn({
+    Key? key,
+    required this.title,
+    required this.press,
+  }) : super(key: key);
+
+  final String title;
+  final VoidCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+      child: Row(
+        children: <Widget>[
+          TitleWithCustomUnderline(text: title),
+          const Spacer(),
+          TextButton(
+            style: TextButton.styleFrom(
+              backgroundColor: kPrimaryColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+            onPressed: press,
+            child: const Text(
+              "More",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
