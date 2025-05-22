eed34693b74056bf4851b2fbe68b54f6fbc4018b
NilaAyu1611 <nilaayucw@gmail.com>
2025-05-23 01:09:16 +0700

    widget utama -> menampilkan daftar tanaman unggulan

---
+++ lib/screens/home/components/featurred_plants.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import '../../../constants.dart';
+
+class FeaturedPlants extends StatelessWidget {
+  const FeaturedPlants({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: <Widget>[
+          FeaturePlantCard(
+            image: "assets/images/bottom_img_1.png",
+            press: () {},
+          ),
+          FeaturePlantCard(
+            image: "assets/images/bottom_img_2.png",
+            press: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
