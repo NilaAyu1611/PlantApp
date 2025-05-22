b98bc9501a8e19096b5aabd49a370255eb2544b7
NilaAyu1611 <nilaayucw@gmail.com>
2025-05-23 02:30:42 +0700

    Tambah fungsi navigasi ke MapPage dan simpan hasil lokasi

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -4,14 +4,35 @@ import 'package:plantapp/screens/home/components/featurred_plants.dart';
 import 'package:plantapp/screens/home/components/header_with_seachbox.dart';
 import 'package:plantapp/screens/home/components/recomend_plants.dart';
 import 'package:plantapp/screens/home/components/tittle_with_more_bbtn.dart';
+import 'package:plantapp/screens/home/components/map_page.dart';
+import 'package:plantapp/screens/home/components/profile_header.dart';
 
-class Body extends StatelessWidget {
-  const Body({super.key});
+class Body extends StatefulWidget {         //akan menampilkan data yang bisa berubah (alamat)
+  
+  @override
+   _BodyState createState() => _BodyState();
+}
+   
+class _BodyState extends State<Body> {
+  String? selectedAddress;
+
+  void _openMapPage() async {
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const MapPage()),
+    );
+
+    if (result != null && result is String) {
+      setState(() {
+        selectedAddress = result;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    // Mendapatkan ukuran layar
-    final Size size = MediaQuery.of(context).size;
+    Size size = MediaQuery.of(context).size;
+
 
     // Mengembalikan widget scrollable
     return SingleChildScrollView(
