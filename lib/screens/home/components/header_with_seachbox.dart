d444fee2c2ab3093c80d21b7645d3c44c99bc887
NilaAyu1611 <nilaayucw@gmail.com>
2025-05-23 00:31:22 +0700

    mebuat seachbox untuk logo dan pembuka

---
+++ lib/screens/home/components/header_with_seachbox.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+import '../../../constants.dart';
+
+class HeaderWithSearchBox extends StatelessWidget {
+  const HeaderWithSearchBox({
+    super.key,
+    required this.size,
+  });
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: kDefaultPadding * 2.5),
+      height: size.height * 0.2,
+      child: Stack(
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.only(
+              left: kDefaultPadding,
+              right: kDefaultPadding,
+              bottom: 36 + kDefaultPadding,
+            ),
+            height: size.height * 0.2 - 27,
+            decoration: const BoxDecoration(
+              color: kPrimaryColor,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(36),
+                bottomRight: Radius.circular(36),
+              ),
+            ),
+            child: Row(
+              children: <Widget>[
+                Text(
+                  'Hi Uishopy!',
+                  style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                ),
+                const Spacer(),
+                Image.asset("assets/images/logo.png"),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
